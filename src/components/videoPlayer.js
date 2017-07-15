163d318ab04cecd9e4b38b85e0b252643fdcb999
Hack Reactor Student <student@hackreactor.com>
2017-07-14 20:10:15 -0500

    Add template, bindings, and controller with getURL function

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,14 @@
 angular.module('video-player')
-
-.component('videoPlayer', {
-  // TODO
-});
+  .component('videoPlayer', {
+    templateUrl: 'src/templates/videoPlayer.html',
+    bindings: {
+      videos: '<',
+      video: '='
+    },
+    controller: function() {
+      this.getURL = function() {
+        return this.video ? 'https://www.youtube.com/embed/' + this.video.id.videoId : 'https://www.youtube.com/embed/dQw4w9WgXcQ';
+      };
+    } 
+  }
+);
