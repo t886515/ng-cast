1bc5a17d94f149cc3b93e4297bc8418b34debbc9
Hack Reactor Student <student@hackreactor.com>
2017-07-14 20:07:11 -0500

    Add template and controller, add trackers for current videos and video playing, and add a click handler function

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,14 @@
-angular.module('video-player')
-
-.component('app', {
-  // TODO
-});
+angular
+  .module('video-player')
+  .component('app', {
+    templateUrl: 'src/templates/app.html',
+    controller: function() {
+      this.videos = window.exampleVideoData;
+      this.currentVideo = this.videos[0];
+      
+      this.selectVideo = function(video) { this.currentVideo = video; }.bind(this);
+      this.searchResults = function() { return 1; };
+    }
+  }
+  
+);
