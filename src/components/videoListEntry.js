de6afbf7c7b94429cec54b58ef11914b618e95dc
Hack Reactor Student <student@hackreactor.com>
2017-07-14 20:09:19 -0500

    Add template, bindings, controller with a changeCurrentVideo function linked back to App controller

--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,4 +1,15 @@
 angular.module('video-player')
-.component('videoListEntry', {
-  // TODO
-});
+  .component('videoListEntry', {
+    templateUrl: 'src/templates/videoListEntry.html',
+    bindings: {
+      video: '<',
+      current: '=',
+      onClick: '<'
+    },
+    controller: function() {
+      this.changeCurrentVideo = function(video) {
+        this.onClick(video);
+      };
+    }
+  }
+);
